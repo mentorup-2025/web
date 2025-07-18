e972adfc9e1280b1610e61fb9ae08bad14e4dc1f
Benson Koon <bensonguan10@gmail.com>
2025-07-18 15:51:19 -0700

    allow api/meet/generate to generate meet link

---
+++ app/types/meet.ts
@@ -0,0 +1,9 @@
+export interface Meet {
+  meeting_link: string;
+  calendar_link: string;
+  start_time: string;
+  end_time: string;
+  duration_minutes: number;
+  mentor_email: string;
+  mentee_email: string;
+} 
